998cb5c70257ef1ea4882742c070b48171b91c49
oreolion <remyoreo11@gmail.com>
2024-09-09 06:22:52 -0700

    setup routing;

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="">
       <Navbar></Navbar>
       <div className="h-[18rem]"></div>
+      <Button>Click me</Button>
       <Footer></Footer>
     </div>
   );
