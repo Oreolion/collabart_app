1897eea75960421d8e346817adbe879217a6b938
oreolion <remyoreo11@gmail.com>
2024-09-11 04:57:27 -0700

    add about section;

---
+++ components/SecondCarousel.tsx
@@ -0,0 +1,74 @@
+'use client'
+import React, { useState, useEffect, useCallback } from 'react'
+import { EmblaOptionsType } from 'embla-carousel'
+import useEmblaCarousel from 'embla-carousel-react'
+import { Thumb } from './EmblaCarouselThumbsAction'
+import styles from "../styles/emblacarousel.module.css";
+
+
+type PropType = {
+  slides: number[]
+  options?: EmblaOptionsType
+}
+
+const SecondCarousel: React.FC<PropType> = (props) => {
+  const { slides, options } = props
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [emblaMainRef, emblaMainApi] = useEmblaCarousel(options)
+  const [emblaThumbsRef, emblaThumbsApi] = useEmblaCarousel({
+    containScroll: 'keepSnaps',
+    dragFree: true
+  })
+
+  const onThumbClick = useCallback(
+    (index: number) => {
+      if (!emblaMainApi || !emblaThumbsApi) return
+      emblaMainApi.scrollTo(index)
+    },
+    [emblaMainApi, emblaThumbsApi]
+  )
+
+  const onSelect = useCallback(() => {
+    if (!emblaMainApi || !emblaThumbsApi) return
+    setSelectedIndex(emblaMainApi.selectedScrollSnap())
+    emblaThumbsApi.scrollTo(emblaMainApi.selectedScrollSnap())
+  }, [emblaMainApi, emblaThumbsApi, setSelectedIndex])
+
+  useEffect(() => {
+    if (!emblaMainApi) return
+    onSelect()
+
+    emblaMainApi.on('select', onSelect).on('reInit', onSelect)
+  }, [emblaMainApi, onSelect])
+
+  return (
+    <div className={styles.embla}>
+      <div className={styles.embla__viewport} ref={emblaMainRef}>
+        <div className={styles.embla__container}>
+          {slides.map((index) => (
+            <div className={styles.embla__slide} key={index}>
+              <div className={styles.embla__slide__number}>{index + 1}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className={styles.emblathumbs}>
+        <div className={styles.emblathumbs__viewport} ref={emblaThumbsRef}>
+          <div className={styles.emblathumbs__container}>
+            {slides.map((index) => (
+              <Thumb
+                key={index}
+                onClick={() => onThumbClick(index)}
+                selected={index === selectedIndex}
+                index={index}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SecondCarousel
