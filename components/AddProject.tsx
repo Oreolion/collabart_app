268c3a89583c72e7a7bbf33903152a77d6d87c3a
oreolion <remyoreo11@gmail.com>
2024-09-23 02:09:04 -0700

    update to create project page;

--- components/AddProject.tsx
+++ components/AddProject.tsx
@@ -26,8 +26,9 @@ import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { cn } from "@/lib/utils";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Textarea } from "@/components/ui/textarea";
+
 import { Loader } from "lucide-react";
 import { Id } from "@/convex/_generated/dataModel";
 import { useToast } from "@/hooks/use-toast";
@@ -36,12 +37,14 @@ import { api } from "@/convex/_generated/api";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
+import CollaborationAgreement from "./CollaborationAgreement";
 
 export default function AddProject() {
   const [audioStorageId, setAudioStorageId] = useState<Id<"_storage"> | null>(
     null
   );
   const [audioUrl, setAudioUrl] = useState("");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   //   const [audioDuration, setAudioDuration] = useState(0);
   const [imageUrl, setImageUrl] = useState("");
   const [imageStorageId, setImageStorageId] = useState<Id<"_storage"> | null>(
@@ -64,6 +67,14 @@ export default function AddProject() {
   const { toast } = useToast();
   const router = useRouter();
   const createProject = useMutation(api.projects.createProject);
+
+  useEffect(() => {
+    if (isDropdownOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [isDropdownOpen]);
 
   const formSchema = z.object({
     projectTitle: z.string().min(2, "Title must be at least 2 characters"),
@@ -344,6 +355,7 @@ export default function AddProject() {
             <Select
               value={projectAuditionPrivacy}
               onValueChange={(value) => setProjectAuditionPrivacy(value)}
+              onOpenChange={setIsDropdownOpen} // Track when dropdown is open
             >
               <SelectTrigger
                 className={cn(
@@ -356,7 +368,7 @@ export default function AddProject() {
                 />
               </SelectTrigger>
               <SelectContent className="text-16 border-none bg-slate-700 font-bold text-white-1 focus:ring-orange-1 max-h-[40vh] overflow-y-auto">
-                <ScrollArea className="h-full overflow-y-auto">
+                <div className="h-full overflow-y-auto">
                   {projectAuditionPrivacies.map((type) => {
                     return (
                       <SelectItem
@@ -368,7 +380,7 @@ export default function AddProject() {
                       </SelectItem>
                     );
                   })}
-                </ScrollArea>
+                </div>
               </SelectContent>
             </Select>
           </div>
@@ -457,209 +469,9 @@ export default function AddProject() {
             >
               Select a joint-work collaboration agreement for this project:
             </Label>
-            <Select
-              value={collaborationAgreement}
-              onValueChange={(value) => setCollaborationAgreement(value)}
-            >
-              <SelectTrigger id="collaboration-agreement">
-                <SelectValue placeholder="Songwriters Agreement" />
-              </SelectTrigger>
-              <SelectContent className="text-16 max-w-[50rem] border-none bg-slate-700 font-bold text-white-1 focus:ring-orange-1 max-h-[30vh] overflow-y-auto">
-                <ScrollArea className="h-full overflow-y-auto">
-                  <SelectItem value="songwriters">
-                    <h3 className="text-lg font-semibold">
-                      Joint Work Collaboration Agreement
-                    </h3>
-                    <Card className="bg-gray-50">
-                      <CardContent className="p-4 text-sm">
-                        <p>
-                          Standard Songwriter Agreement (project manager
-                          version). Version 2. Last updated 24 January, 2016.
-                        </p>
-                        <p className="mt-2">
-                          I&apos;m a songwriter (or I represent a songwriter)
-                          and have a song that I would like to produce in
-                          collaboration with others. I understand and accept
-                          that others may offer to collaborate with me on my
-                          song either by submitting an audition for my
-                          consideration, or otherwise by accepting an invitation
-                          directly from me. Acceptance of any contribution to my
-                          project will remain at my sole discretion as the
-                          project manager.
-                        </p>
-                      </CardContent>
-                    </Card>
-                  </SelectItem>
-
-                  <SelectItem value="fair-share">
-                    <h3 className="text-lg font-semibold">
-                      Joint Work Collaboration Agreement
-                    </h3>
-
-                    <p>
-                      Standard: Fair Shares Agreement (project manager version).
-                      Version 2. Last updated 24 January, 2016
-                    </p>
-                    <p>
-                      I am starting a new collaboration and intend to share in
-                      the resulting &apos;songwriting&apos; and &apos;sound
-                      recording&apos; copyright ownership. The assignment of
-                      these shares will be at my discretion but will be based on
-                      fair and unbiased consideration of each
-                      collaborator&apos;s individual contribution, as well as
-                      the time and effort invested into the completed song. I
-                      understand and accept that others may offer to collaborate
-                      with me on the song either by submitting an audition for
-                      my consideration, or otherwise by accepting an invitation
-                      directly from me. Acceptance of any collaboration on the
-                      project will remain at my sole discretion as the project
-                      manager. As the project manager, I understand that
-                      collaborators whom I have accepted have the right to leave
-                      my project at any time prior to the song&apos;s
-                      completion. In this case, I agree not to use, and to
-                      remove from my project any sound recording and any written
-                      music or lyric that has been contributed by the
-                      collaborator unless otherwise agreed in writing. I
-                      understand, agree, and accept that after such time that
-                      the project is completed, all individual contributions
-                      used in the writing of the song will become joined as
-                      inseparable or interdependent parts of a unitary whole -
-                      which is legal jargon to say that songwriters will jointly
-                      own &apos;the song&apos;; whilst those involved in the
-                      production of the song will jointly own the sound
-                      recording. Prior to the completion of the project,
-                      collaborators shall retain 100% of the
-                      authorship/ownership of their original work/contribution.
-                      A project is not considered to be &apos;complete&apos;
-                      until a) the project manager closes the project so that no
-                      further changes can be made to the song, and b) the
-                      song&apos;s completion and shares in the song are agreed
-                      by its collaborators. As an outcome of this collaboration:
-                      Shares in the copyright of the composition (music and
-                      lyrics) will be agreed and divided fairly between
-                      songwriters; Shares in the copyright of the master sound
-                      recording will be agreed and divided fairly between those
-                      involved in the production of the recording (e.g.
-                      performers and mix engineers). Wherever practical, the
-                      exact assignment of shares will be agreed as early as
-                      possible within the project and prior to the project being
-                      closed. I understand that I am bound by the terms and
-                      conditions of my membership on ProCollabs which includes a
-                      commitment to respecting the legal, ethical, and moral
-                      rights of all members and copyright owners, and in
-                      particular, those of my collaborators.
-                    </p>
-                  </SelectItem>
-                  <SelectItem value="equal-shares">
-                    <h3 className="text-lg font-semibold">
-                      Joint Work Collaboration Agreement
-                    </h3>
-
-                    <p>
-                      Standard: Equal Shares Agreement (project manager
-                      version). Version 2. Last updated 24 January, 2016
-                    </p>
-                    <p>
-                      I am starting a new collaboration and intend to share
-                      equally in the resulting &apos;songwriting&apos; and
-                      &apos;master sound recording&apos; copyright ownership. I
-                      understand and accept that others may offer to collaborate
-                      with me on the song either by submitting an audition for
-                      my consideration, or otherwise by accepting an invitation
-                      directly from me. Acceptance of any collaboration on the
-                      project will remain at my sole discretion as the project
-                      manager. As the project manager, I understand that
-                      collaborators whom I have accepted have the right to leave
-                      my project at any time prior to the song&apos;s
-                      completion. In this case, I agree not to use, and to
-                      remove from my project any sound recording and any written
-                      music or lyric that has been contributed by the
-                      collaborator unless otherwise agreed in writing. I
-                      understand, agree, and accept that after such time that
-                      the project is complete, all individual contributions used
-                      in the song will become joined as inseparable or
-                      interdependent parts of a unitary whole - which is legal
-                      jargon to say: all collaborators will jointly own
-                      &apos;the song&apos;. Prior to the completion of the
-                      project, collaborators shall retain 100% of the
-                      authorship/ownership of their original work/contribution.
-                      A project is not considered to be &apos;complete&apos;
-                      until a) the project manager closes the project so that no
-                      further changes can be made to the song, and b) the
-                      song&apos;s completion and shares in the song are agreed
-                      by its collaborators. As an outcome of this collaboration:
-                      Shares in the copyright of the composition (music and
-                      lyrics) will be divided equally between all songwriters.
-                      Shares in the copyright of the master sound recording will
-                      be divided equally between all recording artists
-                      (performers) and audio engineers. I understand that I am
-                      bound by the terms and conditions of my membership on
-                      ProCollabs which includes a commitment to respecting the
-                      legal, ethical, and moral rights of all members and
-                      copyright owners, and in particular, those of my
-                      collaborators.
-                    </p>
-                  </SelectItem>
-                  <SelectItem value="no-collab">
-                    <h3 className="text-lg font-semibold">
-                      No Collaboration Agreement
-                    </h3>{" "}
-                    {/* Add more agreement text here */}
-                    <p>
-                      You are choosing not to have a collaborator agreement in
-                      place for this project.
-                    </p>
-                    <p>
-                      A collaboration agreement can help to clarify the
-                      intentions of the project manager and to align
-                      expectations of all collaborators prior to commiting
-                      unwarrented time and effort. Having a collaboration
-                      agreement in place can also help to avoid confusion
-                      between collaborators and reduce the risk of disputes
-                      arising due to uncertainty. Please be aware that without a
-                      Collaborator Agreement in place, ProCollabs will not be
-                      able to investigate or provide assistance in the event of
-                      project related collaboration disputes, should they arise.
-                      &apos;Nuff said. You will manage this project soley in
-                      accordance with applicable copyright laws and in
-                      accordance with the terms and conditions of membership on
-                      CollabArt&apos;s, which includes a commitment to
-                      respecting the legal, ethical, and moral rights of all
-                      members and copyright owners.
-                    </p>
-                  </SelectItem>
-                  <SelectItem value="custom">
-                    Customize Agreement
-                    {/* Add more agreement text here */}
-                  </SelectItem>
-                </ScrollArea>
-              </SelectContent>
-            </Select>
+           <CollaborationAgreement></CollaborationAgreement>
           </div>
-          <div className="space-y-2">
-            <h3 className="text-lg font-semibold">
-              Joint Work Collaboration Agreement
-            </h3>
-            <Card className="bg-gray-50">
-              <CardContent className="p-4 text-sm">
-                <p>
-                  Standard Songwriter Agreement (project manager version).
-                  Version 2. Last updated 24 January, 2016.
-                </p>
-                <p className="mt-2">
-                  I&apos;m a songwriter (or I represent a songwriter) and have a
-                  song that I would like to produce in collaboration with
-                  others. I understand and accept that others may offer to
-                  collaborate with me on my song either by submitting an
-                  audition for my consideration, or otherwise by accepting an
-                  invitation directly from me. Acceptance of any contribution to
-                  my project will remain at my sole discretion as the project
-                  manager.
-                </p>
-                {/* Add more agreement text here */}
-              </CardContent>
-            </Card>
-          </div>
+         
 
           <div className="mt-10 w-full">
             <Button
